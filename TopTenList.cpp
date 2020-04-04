8496744736d5f2fb929b6f8671bdac4624deab96
Mitch McFarland <mitch@pop-os.localdomain>
2020-04-03 20:59:50 -0700

    Add fully functional TopTenList source and header files

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,21 +1,36 @@
 //Manages a list of top ten hyperlinks
+#include <iostream>
 #include <string>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
 using std::string;
+using std::cout;
+using std::endl;
 
 TopTenList::TopTenList()
 {
-
+    _list.resize(10);
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+   _list[index-1] = link; 
 }
 
 Hyperlink TopTenList::get(int index)
 {
+    return _list[index-1];
+}
 
+void TopTenList::display_forward()
+{
+    for(auto link : _list)
+        cout << link.text << endl; 
+}
+
+void TopTenList::display_backward()
+{
+    for(int i=9; i>=0; i--) 
+        cout << _list[i].text << endl;
 }
